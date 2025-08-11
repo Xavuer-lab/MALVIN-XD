085844024721e4e17f4876b5774c74087f633cde
Xavuer-lab <sopranoestbeau@gmail.com>
2025-08-11 23:53:08 +0000

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~V3MFRQLQ#WYAgQ2IloeHN-3EeFTBCIeVMEQfNnZFjbMtZdMLaftg",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "2250141137084", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
