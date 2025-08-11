05accbb659e1119a93387eb12836a294f12127be
Xavuer-lab <sopranoestbeau@gmail.com>
2025-08-11 23:28:37 +0000

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~0rkHXRTD#O48vt0_OhfSaVIWx2Us52xGOjTt2YHh448WmarYhPzI",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "2250141137084", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
